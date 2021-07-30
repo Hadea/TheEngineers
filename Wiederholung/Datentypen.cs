f403a1534b2826eb4f46dcae215cfd4bfcd0430a
Lutz <lambda2@gmx.at>
2021-07-30 09:33:37 +0200

    Add project files.

---
+++ Wiederholung/Datentypen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wiederholung
+{
+    class Datentypen
+    {
+        // Ganze zahlen
+
+        int GanzeZahl4ByteMitVorzeichen; // -2,14 Milliarden bis +2,14 Milliarden, häufigster Datentyp
+        uint GanzeZahl4ByteOhneVorzeichen; // 0 bis +4,29 Milliarden  "unsigned integer"
+
+        short GanzeZahl2ByteMitVorzeichen; // -32 Tausend bis +32 Tausend
+        ushort GanzeZahl2ByteOhneVorzeichen; // 0 bis 65535 , beliebt bei Netzwerk
+
+        byte GanzeZahl1ByteOhneVorzeichen; // 0 bis 255
+        sbyte GanzeZahl1ByteMitVorzeichen; // -128 bis +127
+
+    }
+}
