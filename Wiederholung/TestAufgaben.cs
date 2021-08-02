58c730483f18085147440c14a1fc36ef4c415e59
Lutz <lambda2@gmx.at>
2021-08-02 11:04:33 +0200

    added class for excercises

---
+++ Wiederholung/TestAufgaben.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wiederholung
+{
+    class TestAufgaben
+    {
+        public static void RaufzaehlenV1()
+        {
+            for (int counter = 3; counter < 22; counter += 3)
+            {
+                Console.Write("Zahl " + counter + " ist ");
+
+                if (counter % 2 == 0)
+                {
+                    Console.WriteLine("gerade");
+                }
+                else
+                {
+                    Console.WriteLine("ungerade");
+                }
+            }
+            /*
+
+            Zahl 3 ist ungerade
+            Zahl 6 ist gerade
+            Zahl 9 ist ungerade
+            Zahl 12 ist gerade
+            Zahl 15 ist ungerade
+            Zahl 18 ist gerade
+            Zahl 21 ist ungerade
+
+             */
+
+        }
+
+        public static void RaufzaehlenV2()
+        {
+            for (int counter = 3; counter < 22; counter += 3)
+                Console.WriteLine("Zahl " + counter + " ist " + (counter % 2 == 0 ? "gerade" : "ungerade"));
+        }
+    }
+}
