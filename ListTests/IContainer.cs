a1cf75b2c72ee58ce08874ff38e74e3c9cda6a07
Lutz <lambda2@gmx.at>
2021-08-24 16:29:40 +0200

    implemented second batch of tests with resize

--- ListTests/IContainer.cs
+++ ListTests/IContainer.cs
@@ -2,8 +2,8 @@
 {
     interface IContainer
     {
-        public int Count {  get; protected set; }
-        public int Capacity {  get; set; }
+        public int Count { get; }
+        public int Capacity { get; set; }
         public void Add(int Number);
         public int Get(int Index);
     }
--- ListTests/List.cs
+++ ListTests/List.cs
@@ -1,23 +1,58 @@
-﻿namespace ListTests
+﻿using System;
+
+namespace ListTests
 {
     internal class List : IContainer
     {
-        private int[] data;
+        protected int[] data;
+
         public List()
         {
-            data = new int[100];
+            data = new int[8];
         }
 
-        public int Count = 0;
+
+        public int Capacity
+        {
+            get => data.Length;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        public int Count
+        {
+            get => _count;
+        }
+        protected int _count;
 
         public void Add(int Number)
         {
+            if (Count == Capacity) // prüfen ob wir noch platz haben
+            {
+                int[] resizedArray = new int[Capacity * 2];
+                for (int i = 0; i < data.Length; i++)
+                {
+                    resizedArray[i] = data[i];
+                }
+                data = resizedArray;
+            }
+
             data[Count] = Number;
-            Count++;
+            _count++;
         }
 
         public int Get(int Index)
         {
+            if (Index >= Count || Index < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
             return data[Index];
         }
     }
