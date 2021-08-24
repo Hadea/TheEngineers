8a4a08fa66e58cde4efd7159a9ce5ed6afcd8f05
Lutz <lambda2@gmx.at>
2021-08-24 13:38:55 +0200

    added solution for list exercise

---
+++ ListTests/List.cs
@@ -0,0 +1,24 @@
+﻿namespace ListTests
+{
+    internal class List : IContainer
+    {
+        private int[] data;
+        public List()
+        {
+            data = new int[100];
+        }
+
+        public int Count = 0;
+
+        public void Add(int Number)
+        {
+            data[Count] = Number;
+            Count++;
+        }
+
+        public int Get(int Index)
+        {
+            return data[Index];
+        }
+    }
+}
