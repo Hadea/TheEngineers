fd3658d1cfd3fa62778b557bebd04fd97fa91111
Lutz <lambda2@gmx.at>
2021-08-24 14:49:20 +0200

    refined list tests with resize and exceptions

--- ListTests/IContainer.cs
+++ ListTests/IContainer.cs
@@ -2,6 +2,8 @@
 {
     interface IContainer
     {
+        public int Count {  get; protected set; }
+        public int Capacity {  get; set; }
         public void Add(int Number);
         public int Get(int Index);
     }
--- ListTests/ListTest.cs
+++ ListTests/ListTest.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace ListTests
 {
@@ -58,6 +59,67 @@ namespace ListTests
             Assert.AreEqual(4, l.Get(1));
             Assert.AreEqual(6, l.Get(2));
             Assert.AreEqual(8, l.Get(3));
+        }
+
+        [TestMethod]
+        public void GetCapacity()
+        {
+            List l = new List();
+            Assert.AreEqual(8, l.Capacity);
+        }
+
+        [TestMethod]
+        public void GetCapacityAfterResize()
+        {
+            List l = new List();
+            for (int i = 0; i < 12; i++)
+            {
+                l.Add(i);
+            }
+            Assert.AreEqual(16, l.Capacity);
+            Assert.AreEqual(12, l.Count);
+        }
+
+        [TestMethod]
+        public void GetContentsAfterResize()
+        {
+            List l = new List();
+            Assert.AreEqual(8, l.Capacity);
+            for (int i = 0; i < 12; i++)
+            {
+                l.Add(i);
+            }
+            Assert.AreEqual(16, l.Capacity);
+
+            for (int i = 0; i < 12; i++)
+            {
+                Assert.AreEqual(i, l.Get(i));
+            }
+        }
+
+        [TestMethod]
+        public void ExceptionNegativeCapacity()
+        {
+            List l = new List();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => l.Capacity = 0);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => l.Capacity = -1);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => l.Capacity = -22);
+        }
+        
+        [TestMethod]
+        public void ExceptionNegativeIndex()
+        {
+            List l = new List();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => l.Get(-1));
+        }
+        
+        [TestMethod]
+        public void ExceptionIndexTooHigh()
+        {
+            List l = new List();
+            l.Add(1);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => l.Get(1));
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => l.Get(2));
         }
     }
 }
