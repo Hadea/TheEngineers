710022f2e4b62ee9dcb9113d5331038a8b085e9d
Lutz <lambda2@gmx.at>
2021-08-25 16:52:44 +0200

    added generics example

---
+++ Wiederholung/Generics.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Wiederholung
+{
+    class Generics<DataType> // erstellt eine Klasse welche für jeden Datentypen (nur die benutzten) eine Kopie für diesen datentypen erstellt
+    {
+        DataType data; // Der Platzhalter "DataType" wird beim kompilieren ersetzt durch den gewünschten datentypen
+    }
+
+    class ClassWithGenericMethod
+    {
+        DataType GenericMethod<DataType>(DataType parameter) // auch einzelne Methoden können ein Generic sein. Für jeden benötigte Variante wird dann vom Compiler eine Überladung erzeugt
+        {
+            return parameter;
+        }
+    }
+
+
+}
