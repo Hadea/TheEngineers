c3084c5169fc4733d9acb5ba7aa30472ca09f8aa
Lutz <lambda2@gmx.at>
2021-08-25 16:53:56 +0200

    added conversion example with tryparse

---
+++ Wiederholung/InputConversion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wiederholung
+{
+    class InputConversion
+    {
+        public static void DoSomething()
+        {
+            List<int> aLotOfNumbers = new List<int>();
+
+            string userInput = Console.ReadLine();
+
+            int convertedNumber;
+
+            do
+            {
+                Console.WriteLine("Bitte Zahl eingeben");
+            } while (int.TryParse(userInput, out convertedNumber));
+            Console.WriteLine("Zahl akzeptiert");
+            aLotOfNumbers.Add(convertedNumber);
+        }
+    }
+}
