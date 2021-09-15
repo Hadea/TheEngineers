6eaa45a8f678e903427ac8956f4b8ce0b071d1b7
Lutz <lambda2@gmx.at>
2021-09-15 10:44:59 +0200

    Added interface for Logic

---
+++ TicTacToeLogic/ITicTacToeLogic.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TicTacToeLogic
+{
+    public interface ITicTacToeLogic
+    {
+        public bool CurrentPlayer { get; set; }
+        public Field[,] GameBoard { get; init; }
+        public TurnResult Turn(int X, int Y);
+    }
+}
--- TicTacToeLogic/Logic.cs
+++ TicTacToeLogic/Logic.cs
@@ -3,13 +3,77 @@ using System.Collections.Generic;
 
 namespace TicTacToeLogic
 {
-    public class Logic
+    public class Logic : ITicTacToeLogic
     {
-        public Field[,] GameBoard;
+        public Field[,] GameBoard { get; init; }
+        public bool CurrentPlayer { get; set; }
+        public bool GameRunning { get; protected set; }
+        protected int turnNumber;
 
-        public bool GetCurrentPlayer() { return false; }
-        public TurnResult Turn(int X, int Y) { return TurnResult.Invalid; }
-        protected bool checkWin() { return true; }
+        public Logic()
+        {
+            GameBoard = new Field[3, 3];
+            ResetGame();
+        }
+        
+        public TurnResult Turn(int X, int Y)
+        {
+            if (X < 0 || X > 2 || Y < 0 || Y > 2 || GameBoard[Y, X] != Field.Empty || !GameRunning)
+                return TurnResult.Invalid;
+
+            Field playerStone = (CurrentPlayer ? Field.X : Field.O);
+            GameBoard[Y, X] = playerStone;
+            turnNumber++;
+
+            if (checkWin(playerStone))
+            {
+                GameRunning = false;
+                return TurnResult.Win;
+            }
+            else
+            {
+                if (turnNumber < 9)
+                {
+                    CurrentPlayer = !CurrentPlayer;
+                    return TurnResult.Valid;
+                }
+                else
+                {
+                    GameRunning = false;
+                    return TurnResult.Draw;
+                }
+            }
+        }
+
+        protected bool checkWin(Field playerStone)
+        {
+            return
+                // Horizontal
+                (GameBoard[0, 0] == playerStone && playerStone == GameBoard[0, 1] && playerStone == GameBoard[0, 2]) ||
+                (GameBoard[1, 0] == playerStone && playerStone == GameBoard[1, 1] && playerStone == GameBoard[1, 2]) ||
+                (GameBoard[2, 0] == playerStone && playerStone == GameBoard[2, 1] && playerStone == GameBoard[2, 2]) ||
+                // Vertikal
+                (GameBoard[0, 0] == playerStone && playerStone == GameBoard[1, 0] && playerStone == GameBoard[2, 0]) ||
+                (GameBoard[0, 1] == playerStone && playerStone == GameBoard[1, 1] && playerStone == GameBoard[2, 1]) ||
+                (GameBoard[0, 2] == playerStone && playerStone == GameBoard[1, 2] && playerStone == GameBoard[2, 2]) ||
+                // Diagonal
+                (GameBoard[0, 0] == playerStone && playerStone == GameBoard[1, 1] && playerStone == GameBoard[2, 2]) ||
+                (GameBoard[0, 2] == playerStone && playerStone == GameBoard[1, 1] && playerStone == GameBoard[2, 0]);
+        }
+
         public List<Score> GetHighscore() { return new List<Score>(); }
+
+        public void ResetGame()
+        {
+            for (int Y = 0; Y < 3; Y++)
+            {
+                for (int X = 0; X < 3; X++)
+                {
+                    GameBoard[Y, X] = Field.Empty;
+                }
+            }
+            GameRunning = true;
+            turnNumber = 0;
+        }
     }
 }
