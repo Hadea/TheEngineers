89e2e39fc24c50620338d85a29e3f91dc7641ef9
Lutz <lambda2@gmx.at>
2021-09-16 14:38:07 +0200

    added demo for using

--- Wiederholung/Program.cs
+++ Wiederholung/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main()
         {
-            Dateien.DoSomething();
+            UsingDemo.DoSomething();
         }
     }
 }
--- Wiederholung/UsingDemo.cs
+++ Wiederholung/UsingDemo.cs
@@ -8,32 +8,43 @@ namespace Wiederholung
         {
             using (RessourcenKlasse resource = new())
             {
-                // operationen auf der Ressource
-                // operationen auf der Ressource
-                // operationen auf der Ressource
-                // operationen auf der Ressource
 
-            } //resource.Close();
+                // operationen auf der Ressource
+                // operationen auf der Ressource
+                // operationen auf der Ressource
+                // operationen auf der Ressource
+            }
+
+            //resource.Close();
+            //resource.Dispose();
         }
     }
 
     class RessourcenKlasse : IDisposable
     {
+        bool ressourcenBereitsFreigegeben;
+
         public RessourcenKlasse()
         {
+            Console.WriteLine("Reserviere die Ressourcen");
             // reserviert die ressourcen
+            // z.B. baut eine Netzwerkverbindung auf
         }
 
         public void Close()
         {
+            if (ressourcenBereitsFreigegeben)
+                return;
+            Console.WriteLine("Gebe die Ressourcen frei");
+            ressourcenBereitsFreigegeben = true;
             // gibt die ressourcen wieder frei
+            // z.B. trennt eine Netzwerkverbindung
         }
 
         public void Dispose() // wird vom using aufgerufen sobald die Klammern verlassen werden
         {
+            Console.WriteLine("es gibt keine ressourcen");
             Close();
         }
     }
-
-
 }
