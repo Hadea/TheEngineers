ee43f1ba88d1a1512dd72975257e5df8a2763360
Lutz <lambda2@gmx.at>
2021-09-16 09:31:01 +0200

    added demo for using

---
+++ Wiederholung/UsingDemo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Wiederholung
+{
+    class UsingDemo
+    {
+        public static void DoSomething()
+        {
+            using (RessourcenKlasse resource = new())
+            {
+                // operationen auf der Ressource
+                // operationen auf der Ressource
+                // operationen auf der Ressource
+                // operationen auf der Ressource
+
+            } //resource.Close();
+        }
+    }
+
+    class RessourcenKlasse : IDisposable
+    {
+        public RessourcenKlasse()
+        {
+            // reserviert die ressourcen
+        }
+
+        public void Close()
+        {
+            // gibt die ressourcen wieder frei
+        }
+
+        public void Dispose() // wird vom using aufgerufen sobald die Klammern verlassen werden
+        {
+            Close();
+        }
+    }
+
+
+}
