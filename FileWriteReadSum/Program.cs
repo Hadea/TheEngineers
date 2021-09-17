7bce16d79ba58880384121f02c688a27aee76cb8
Lutz <lambda2@gmx.at>
2021-09-17 13:13:49 +0200

    added exercise for file handling

---
+++ FileWriteReadSum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FileWriteReadSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Zwei zahlen von der Konsole lesen
+             * Die beiden Zahlen in einer Datei ablegen
+             * Die Zahlen aus der Datei lesen
+             * Die summe der Zahlen ausgeben
+             */
+
+            /*
+             * Begrüssung ausgeben
+             * Nach erster Zahl fragen
+             * Nach zweiter Zahl fragen
+             * Datei öffnen mit schreibrechten
+             * Datei befüllen mit beiden Zahlen
+             * Datei schliessen
+             * Datei öffnen mit leserechten
+             * Erste Zahl lesen und zwischenspeichern
+             * Zweite Zahl lesen und zwischenspeichern
+             * Summe aus beiden Zahlen ausgeben
+             */
+        }
+    }
+}
